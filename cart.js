0aeac935e7205ae5499c4a3b258c00de0385280b
Cream <marstarty3631@gmail.com>
2023-02-26 13:37:45 +0800

    message

--- cart.js
+++ cart.js
@@ -1,12 +1,15 @@
-import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+// import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
 
 const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
 const { required, email, min, max } = VeeValidateRules;
 const { localize, loadLocaleFromURL } = VeeValidateI18n;
 
 console.log(VeeValidate);
+console.log(VeeValidateRules);
 VeeValidate.defineRule('email', VeeValidateRules['email']);
 VeeValidate.defineRule('required', VeeValidateRules['required']);
+VeeValidate.defineRule('min', VeeValidateRules['min']);
+VeeValidate.defineRule('max', VeeValidateRules['max']);
 
 // //定義規則
 // Object.keys(VeeValidateRules).forEach(rule => {
@@ -58,45 +61,71 @@ const productModal={
                     console.log(this.tempProduct);
                     this.modal.show();
                 })
+                .catch((err)=>{
+                    console.log(err.data.message);
+                })
             }
         }
     },
     methods:{
         hide(){
             this.modal.hide();
+          
+
         }
     },
     mounted(){
-        this.modal = new bootstrap.Modal(this.$refs.modal)
+        this.modal = new bootstrap.Modal(this.$refs.modal) //使用bs5 Modal實體取得
         //監聽DOM，當Modal關閉時，要清空id
         this.$refs.modal.addEventListener('hidden.bs.modal', function (event) {
             console.log("被關閉");
             this.openModal('');
+            
         })
         
     }
 }
 
-const app=createApp({
+const app=Vue.createApp({
     data(){
         return{
             products:[],
             productId:"",
             cart:{},
             loadingItem:"",//存id
+            form: {
+                user: {
+                  name: '',
+                  email: '',
+                  tel: '',
+                  address: '',
+                },
+                message: '',
+            },
         }
     },
     methods:{
+        onSubmit() {
+            console.log(this.form);
+        },
+        isPhone(value){
+            const phoneNumber = /^(09)[0-9]{8}$/
+            return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+        },
         getProducts(){
             axios.get(`${url}/api/${path}/products/all`)
                 .then((res)=>{
                     console.log('產品列表',res.data.products);
                     this.products=res.data.products;
                     console.log(this.products);
-            })
+                 })
+                 .catch((err)=>{
+                    console.log(err.data.message);
+                })
         },
         openModal(id){
             this.productId=id;
+            this.loadingItem=id;
             console.log("外層傳入",this.productId);
         },
         addToCart(product_id,qty=1){ //當沒有參數傳入帶入預設值
@@ -104,12 +133,17 @@ const app=createApp({
                 product_id, //同名不寫第二次
                 qty
             }
+            this.loadingItem=product_id;
             axios.post(`${url}/api/${path}/cart`,{data})
                 .then((res)=>{
                     console.log('加入購物車:',res.data);
                     this.$refs.productModal.hide();
+                    this.loadingItem="";
                     this.getCarts();
-            })
+                 })
+                 .catch((err)=>{
+                    console.log(err.data.message);
+                })
         },
         getCarts(){
             axios.get(`${url}/api/${path}/cart`)
@@ -117,8 +151,9 @@ const app=createApp({
                 console.log('取得購物車',res.data.data);
                 this.cart=res.data.data;
                 console.log("查看有沒有準確存入",this.cart);
-                
-               
+            })
+            .catch((err)=>{
+                console.log(err.data.message);
             })
         },
         updateCartItem(item){ //購物車的id、產品的id
@@ -133,8 +168,11 @@ const app=createApp({
                     this.getCarts();
                     this.loadingItem="";
                 })
+                .catch((err)=>{
+                    console.log(err.data.message);
+                })
         },
-        deleteCartItem(item){
+        deleteCartItem(item){ //刪除指定品項
             this.loadingItem=item.id;
             axios.delete(`${url}/api/${path}/cart/${item.id}`)
             .then((res)=>{
@@ -142,7 +180,34 @@ const app=createApp({
                 this.getCarts();
                 this.loadingItem="";
             })
-        }
+            .catch((err)=>{
+                console.log(err.data.message);
+            })
+        },
+        deleteCarts(){ //清空購物車
+            axios.delete(`${url}/api/${path}/carts`)
+                .then((res)=>{
+                    console.log(res.data.message);
+                    this.getCarts();
+                })
+                .catch((err)=>{
+                    console.log(err.data.message);
+                    alert(err.data.message);
+                })
+        },
+        createOrder() { //送出結帳
+            const order = this.form;
+            axios.post(`${url}/api/${path}/order`, { data: order })
+                .then((res) => {
+                    alert(res.data.message);
+                    this.$refs.form.resetForm();
+                    this.getCarts();
+                })
+                .catch((err) => {
+                    console.log(err.data.message);
+                    });
+                },
+
     },
     components:{
         productModal
